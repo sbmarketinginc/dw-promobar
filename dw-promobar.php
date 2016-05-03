d2dfefff76c55e1d787996e2812b8f76f4ce2574
Brendon Schenecker <info@sbmarketinginc.com>
2016-05-03 14:06:07 -0400

    Added Temporary URL triggered Coupons Override
    Integrate the stock woo commerce coupons plugin to allow for overrides promo bar settings. General use case www.url.com?promo_code=COUPONCODE sets cookie that shows COUPONCODE info in top bar until cookie expires.

--- dw-promobar.php
+++ dw-promobar.php
@@ -8,6 +8,29 @@ Author: DesignWall
 Author URI: http://www.designwall.com
 */
 
+// Set Promo cookie if present in Url specific to dynamic
+add_action( 'init', 'set_promo_cookie' );
+
+function set_promo_cookie() {
+	if(isset($wp_query->query_vars['promo_code'])) {
+  		setcookie( $promo_code, $wp_query->query_vars['promo_code'], (1 * DAYS_IN_SECONDS));
+	}
+}
+
+// Get Promo Code from cookie or URL - specific to dynamic
+function get_promo_code (){
+	if(!isset($_COOKIE[$promo_code])) {
+		$promo_code = $_COOKIE[$promo_code];
+	} elseif (isset($wp_query->query_vars['promo_code'])) {
+		$promo_code = $wp_query->query_vars['promo_code'];
+	} else {
+		return null;
+	}
+	return $promo_code;
+}
+
+$promo_code = get_promo_code();
+
 if ( ! function_exists('dwpb') ) {
 
 	define( 'DWPB_FOLDER', plugin_dir_path(__FILE__) );
@@ -247,12 +270,199 @@ if ( ! function_exists('dwpb') ) {
 	endif; // Show on
 	}
 
+// Specific For Dynamic Promos
+	function dwpb_dynamic() {
+		
+		//Get Promo Code as an Object
+		$promo_code = get_promo_code();
+		$promo_object = get_page_by_title( $promo_code, 'OBJECT', 'shop_coupon' );
+		
+		$dwpb_d_bar_text = $promo_object->post_excerpt;
+		$dwpb_d_link_text = $promo_object->post_title;
+		$dwpb_d_link_url = '/shop/';
+		$dwpb_d_link_target = '_self';
+		
+		$current_theme = wp_get_theme();
+		$is_front_page = dwpb_get_option('dwpb_front_page', false);
+		$is_archives = dwpb_get_option('dwpb_archives', false);
+		$is_tags = dwpb_get_option('dwpb_tags', false);
+		$is_single_post = dwpb_get_option('dwpb_single_post', false);
+		$is_single_page = dwpb_get_option('dwpb_single_page', false);
+		if ( 
+			( $is_front_page && is_front_page() ) || 
+			( $is_archives && is_archive() ) || 
+			( $is_tags && is_tag() ) ||
+			( $is_single_post && is_single() ) ||
+			( $is_single_page && is_page() ) ||
+			( ! $is_front_page && ! $is_archives && ! $is_tags && ! $is_single_post && ! $is_single_page ) || 
+			is_admin()
+		) :
+		
+		$dwpb_ramain_top = dwpb_get_option('dwpb_ramain_top');
+		$dwpbcd_use = dwpb_get_option('dwpbcd_use');
+
+		$dwpbcd_link_text = $dwpb_d_link_text;
+		$dwpbcd_link_url = $dwpb_d_link_url;
+		$dwpbcd_link_target = $dwpb_d_link_target;
+
+		$dwpb_link_text = $dwpbd_link_text;
+		$dwpb_link_url = $dwpbd_link_url;
+		$dwpb_link_target = $dwpbd_link_target;
+
+		$dwpb_font_family = dwpb_get_option('dwpb_font_family');
+		$dwpb_font_size = dwpb_get_option('dwpb_font_size');
+		$dwpb_background_color = dwpb_get_option('dwpb_background_color');
+		$dwpb_background_image = dwpb_get_option('dwpb_background_image');
+		$dwpb_font_color = dwpb_get_option('dwpb_font_color');
+		$dwpb_border_color = dwpb_get_option('dwpb_border_color');
+		$dwpb_link_color = dwpb_get_option('dwpb_link_color');
+		$dwpb_link_style = dwpb_get_option('dwpb_link_style');
+		$dwpb_button_color = dwpb_get_option('dwpb_button_color');
+
+		$dwpb_custon_style = dwpb_get_option('dwpb_custon_style');
+
+		$dwpb_link = '';
+		if ( $dwpb_link_text != '' ) {
+			$dwpb_link = ' <a class="'. $dwpb_link_style .'" href="'.$dwpb_link_url.' " target="'. $dwpb_link_target .'"" >'.$dwpb_link_text.'</a>';
+		}
+
+		$dwpbcd_link = '';
+		if ( $dwpbcd_link_text != '' ) {
+			$dwpbcd_link = ' <a class="'. $dwpb_link_style .'" href="'.$dwpbcd_link_url.' " target="'. $dwpbcd_link_target .'"" >'.$dwpbcd_link_text.'</a>';
+		}
+	?>
+		<style>
+			<?php 
+				if( $dwpb_font_family != '' ) : 
+				$font_family = explode(':dw:', $dwpb_font_family );
+			?>
+			@font-face {
+        font-family: "<?php echo $font_family[0]; ?>";
+        src: url('<?php echo $font_family[1] ?>');
+      }
+			#dwpb {
+				font-family: <?php echo $font_family[0] ?>;
+			}
+			<?php endif; ?>
+
+			<?php if( $dwpb_background_color != '' ) : ?>
+			#dwpb,
+			.dwpb-action {
+				background-color: <?php echo $dwpb_background_color; ?>;
+			}
+			<?php endif; ?>
+
+			<?php if( $dwpb_background_image != '' ) : ?>
+			#dwpb {
+				background-image: url(<?php echo $dwpb_background_image; ?>);
+				background-position: center;
+				background-size: 100% auto;
+			}
+			<?php endif; ?>
+
+			<?php if( $dwpb_font_color != '' ) : ?>
+			#dwpb,
+			.dwpb-action,
+			body.dwpb-allow-close.dwpb-open .dwpb-close {
+				color: <?php echo $dwpb_font_color; ?>;
+			}
+			<?php endif; ?>
+
+			<?php if( $dwpb_font_size != '' ) : ?>
+			#dwpb {
+				font-size: <?php echo $dwpb_font_size; ?>px;
+			}
+			<?php endif; ?>
+
+			<?php if( $dwpb_font_size > 20 ) : ?>
+			#dwpb {
+				line-height: 1.2;
+			}
+			<?php endif; ?>
+
+			<?php if( $dwpb_border_color != '' ) : ?>
+			#dwpb {
+				border-color: <?php echo $dwpb_border_color; ?>;
+			}
+			<?php endif; ?>
+
+			<?php if( $dwpb_border_color == '' ) : ?>
+			#dwpb {
+				border-width: 0;
+			}
+			<?php endif; ?>
+
+			<?php if( $dwpb_link_color != '' ) : ?>
+			#dwpb a {
+				color: <?php echo $dwpb_link_color; ?>;
+			}
+			<?php endif; ?>
+
+			<?php if( $dwpb_button_color != '' ) : ?>
+			#dwpb .dwpb-button {
+				background-color: <?php echo $dwpb_button_color; ?>;
+			}
+			<?php endif; ?>
+
+			<?php if( $dwpb_custon_style != '' ) : ?>
+				<?php echo $dwpb_custon_style ?>
+			<?php endif; ?>
+		</style>
+		
+		<div id="dwpb" class=" <?php echo $dwpb_ramain_top; ?> ">
+			<div class="dwpb-inner">
+				<?php 
+					$dwpbcd_hide = 'hide';
+					$dwpb_hide = '';
+					if ($dwpbcd_use == 'yes') {
+						$dwpbcd_hide = '';
+						$dwpb_hide = 'hide';
+					}
+
+					$dwpb_bar_text = $dwpb_d_bar_text;
+					if ( $dwpb_bar_text == '' ) {
+						$dwpb_bar_text = __('Hello. Add your message here.','dwpb');
+					}
+
+					$dwpbcd_text = $dwpb_d_bar_text;
+					if ( $dwpbcd_text == '' ) {
+						$dwpbcd_text = __('Hello. Add your message here.','dwpb');
+					}
+				?>
+
+				<div class="dwpb-message <?php echo $dwpb_hide; ?>">
+					<span class="dwpb-content"><?php echo $dwpb_bar_text; ?></span>
+					<?php echo $dwpb_link; ?>
+				</div>
+					
+				<div class="dwpb-countdown <?php echo $dwpbcd_hide; ?>">
+					<div class="dwpb-counter"></div>
+					<span class="dwpbcd-content"><?php echo $dwpbcd_text; ?></span>
+					<?php echo $dwpbcd_link; ?>
+				</div>
+			</div>
+		</div>
+		<?php 
+			$dwpb_close = dwpb_get_option('dwpb_close');
+			$dwpb_action_class = 'dwpb-action';
+			if ($dwpb_close == 'yes') {
+				$dwpb_action_class = 'dwpb-close';
+			}
+		?>
+		<span class="<?php echo $dwpb_action_class; ?>"></span>
+	<?php
+
+	endif; // Show on
+	}
+
 	$dwpb_enable = dwpb_get_option('dwpb_enable');
 	$dwpb_start = strtotime(dwpb_get_option('dwpb_start'));
 	$dwpb_end = strtotime(dwpb_get_option('dwpb_end'));	
 	$dwpb_timezone = strtotime(date_i18n('Y-m-d G:i:s'));
 
-	if ( ( $dwpb_start < $dwpb_timezone && ( $dwpb_timezone < $dwpb_end || $dwpb_end == '' ) ) && $dwpb_enable == 'yes' ) {
+	if($promo_code){
+		add_action( 'wp_footer', 'dwpb_dynamic', 100);
+	} else if ( ( $dwpb_start < $dwpb_timezone && ( $dwpb_timezone < $dwpb_end || $dwpb_end == '' ) ) && $dwpb_enable == 'yes' ) {
 		add_action( 'wp_footer', 'dwpb', 100);
 	}
 	add_action( 'dwpb_previvew', 'dwpb');
@@ -319,7 +529,74 @@ if ( ! function_exists('dwpb') ) {
 
 		endif; // Show on
 	}
-	if ( ( $dwpb_start < $dwpb_timezone && ( $dwpb_timezone < $dwpb_end || $dwpb_end == '' ) ) && $dwpb_enable == 'yes' ) {
+	
+	// Specific For Dynamic Promos
+	function dwpb_scripts_dynamic() {
+		
+		$is_front_page = dwpb_get_option('dwpb_front_page', false);
+		$is_archives = dwpb_get_option('dwpb_archives', false);
+		$is_tags = dwpb_get_option('dwpb_tags', false);
+		$is_single_post = dwpb_get_option('dwpb_single_post', false);
+		$is_single_page = dwpb_get_option('dwpb_single_page', false);
+		if ( 
+			( $is_front_page && is_front_page() ) || 
+			( $is_archives && is_archive() ) || 
+			( $is_tags && is_tag() ) ||
+			( $is_single_post && is_single() ) ||
+			( $is_single_page && is_page() ) ||
+			( ! $is_front_page && ! $is_archives && ! $is_tags && ! $is_single_post && ! $is_single_page )
+		) :
+
+		// Front end
+		wp_enqueue_style( 'dwpb_style', DWPB_PATH . 'assets/css/main.css');
+
+		if ( ! wp_script_is( 'jquery', 'enqueued' )) {
+			wp_enqueue_script( 'jquery');
+		}
+
+		if ( ! wp_script_is( 'jquery.countdown.js', 'enqueued' )) {
+			wp_enqueue_script( 'dwpb_countdown', DWPB_PATH . 'assets/js/vendor/jquery.countdown.js',true);
+		}
+
+		if ( ! wp_script_is( 'jquery.cookie.js', 'enqueued' )) {
+			wp_enqueue_script( 'dwpb_cookie', DWPB_PATH . 'assets/js/vendor/jquery.cookie.js',true);
+		}
+
+		if ( ! wp_style_is( 'dashicons', 'enqueued' ))  {
+			wp_enqueue_style( 'dashicons' );
+		}
+
+		wp_enqueue_script( 
+			'dwpb_script', 
+			DWPB_PATH . 'assets/js/main.js', 
+			array(
+				'jquery',
+				'dwpb_countdown',
+				'dwpb_cookie'
+			),
+			'1.0',
+			true
+		);
+
+		$timeleft = '';
+		if ( dwpb_get_option('dwpbcd_time_left') != '' ) {
+			$timeleft = dwpb_get_option('dwpbcd_time_left');
+		}
+
+		$timezone_format = _x('Y-m-d G:i:s', 'timezone date format');
+		$dwpb_reset_cookie_value = get_option( 'dwpb_reset_cookie', 2 );
+
+		wp_localize_script( 'dwpb_countdown', 'dwpb', array(
+			'timeleft'	=> strtotime($timeleft) - strtotime(date_i18n($timezone_format)),
+			'reset_cookie' => $dwpb_reset_cookie_value
+		));
+
+		endif; // Show on
+	}
+	
+	if($promo_code){
+		add_action( 'wp_footer', 'dwpb_scripts_dynamic');
+	} else if ( ( $dwpb_start < $dwpb_timezone && ( $dwpb_timezone < $dwpb_end || $dwpb_end == '' ) ) && $dwpb_enable == 'yes' ) {
 		add_action( 'wp_footer', 'dwpb_scripts');
 	}
 
